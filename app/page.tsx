54d442b0220445f708e1fc32771268fa7bbf797a
CharlesGPT <chimecharles23@gmail.com>
2026-07-21 00:09:09 -0400

    Added the chatbot

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { About } from '@/components/sections/About'
 import { TestimonialsSlider } from '@/components/sections/TestimonialsSlider'
 import { FAQ } from '@/components/sections/FAQ'
 import { ExploreMenu } from '@/components/sections/ExploreMenu'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -16,6 +17,12 @@ export default function Home() {
         <ExploreMenu />
         <FAQ />
       </main>
+      <Script
+        src="https://pocketreply.tech/embed.js"
+        data-agent-id="93dfe804-f398-48e1-8062-2349963eccec"
+        data-base-url="https://pocketreply.tech"
+        strategy="lazyOnload"
+      />
     </>
   )
 }
